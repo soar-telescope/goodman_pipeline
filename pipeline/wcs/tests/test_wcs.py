fdd972fbe07c8ae6c743950b449d4c637ed5a004
Simon Torres <storres@ctio.noao.edu>
2018-01-08 10:32:32 -0300

    created empty tests for WCS class

---
+++ pipeline/wcs/tests/test_wcs.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+from ..wcs import WCS
+
+
+def test_wcs_read():
+    wcs_instance = WCS()
+    assert 1 == 1
+
+
+def test_wcs_fit():
+    wcs_instance = WCS()
+    assert 1 == 1
