cb6a1ff8590751331db2fb61841e7a2198348b47
Simon Torres <storres@ctio.noao.edu>
2018-04-13 15:12:36 -0300

    added dcr-source as data_files

--- setup.py
+++ setup.py
@@ -134,7 +134,8 @@ setup(
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    data_files=[('dcr-source', ['dcr-source'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
