24d7b1a616cbae86565b729b87024a01c9dde825
Simon Torres <storres@ctio.noao.edu>
2018-04-13 14:23:57 -0300

    removed dcr as a submodule

--- setup.py
+++ setup.py
@@ -134,8 +134,7 @@ setup(
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[('dcr_source', ['dcr'])],
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
