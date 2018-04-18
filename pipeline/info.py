65f496080bad1299c5e9cf0a50f6d2b98a00474f
Simon Torres <storres@ctio.noao.edu>
2018-04-18 11:25:12 -0300

    updated wcs_model_calculator to be able to process a single image.

--- pipeline/info.py
+++ pipeline/info.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-__version__ = '1.0b10'
+__version__ = '0.10.0beta'
 __author__ = 'Simon Torres'
 __date__ = '2016-06-28'
 __email__ = "storres@ctio.noao.edu"
