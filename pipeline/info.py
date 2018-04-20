2d2da8735aad8afd72ebc741751cebb4377f5b34
Simon Torres <storres@ctio.noao.edu>
2018-04-20 17:00:58 -0300

    changed version to 0.10.1rc due to deletion of the interactive mode.

--- pipeline/info.py
+++ pipeline/info.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-__version__ = '0.10.0rc'
+__version__ = '0.10.1rc'
 __author__ = 'Simon Torres'
 __date__ = '2016-06-28'
 __email__ = "storres@ctio.noao.edu"
