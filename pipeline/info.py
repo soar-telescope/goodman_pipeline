b9fc4a5fbecb01a1b30efe30cff3b80b3064ef39
Bruno Quint <b1quint@gmail.com>
2018-04-29 17:25:10 -0300

    Built v1.0.0 in .tar.gz and in .zip formats.

--- pipeline/info.py
+++ pipeline/info.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
-__version__ = '0.10.2rc'
-__author__ = 'Simon Torres'
-__date__ = '2016-06-28'
-__email__ = "storres@ctio.noao.edu"
-__status__ = "Development"
+__version__ = '1.0.0'
+__author__ = 'Simon Torres, Bruno Quint, Cesar Briceño'
+__date__ = '2018-04-30'
+__email__ = "goodman-pipeline@ctio.noao.edu"
+__status__ = "rtw"
