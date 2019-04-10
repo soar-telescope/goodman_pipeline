9538aface1f40d135304a6d2a2c9d284154b0eac
Simon Torres <storres@ctio.noao.edu>
2019-04-10 17:29:49 -0400

    Added test for binning getter and setter in WCS class

--- goodman_pipeline/wcs/tests/test_unittest.py
+++ goodman_pipeline/wcs/tests/test_unittest.py
@@ -21,6 +21,36 @@ class TestWCS(TestCase):
         self.assertRaisesRegex(SystemExit, '1', self.wcs)
         self.assertRaises(SystemExit, self.wcs)
 
+    def test_binning_property_getter(self):
+        self.assertEqual(self.wcs.binning, 1)
+
+    def test_binning_property_setter_linear_model(self):
+        self.wcs.wcs_dict['crval'] = 3514.5662540243
+        self.wcs.wcs_dict['crpix'] = 1.0
+        self.wcs.wcs_dict['cdelt'] = 0.653432383822811
+        self.wcs._linear_solution()
+
+        self.wcs.binning = 2
+
+        self.assertEqual(self.wcs.binning, 2)
+
+    def test_binning_property_setter_non_linear_model(self):
+        self.wcs.wcs_dict['order'] = 3
+        self.wcs.wcs_dict['pmin'] = 1616.37
+        self.wcs.wcs_dict['pmax'] = 3259.98
+        self.wcs.wcs_dict['fpar'] = [5115.64008185559,
+                                     535.515983711607,
+                                     -0.779265625182385]
+        self.wcs._chebyshev()
+
+        self.wcs.binning = 2
+
+        self.assertEqual(self.wcs.binning, 2)
+
+    def test_binning_property_setter_undefined_model(self):
+        with self.assertRaises(NotImplementedError):
+            self.wcs.binning = 2
+
     def test_pm_fitter_undefined_model_and_fitter(self):
         pixel = list(range(100))
         angstrom = list(range(100))
