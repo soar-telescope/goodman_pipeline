915cb3b033c948c0a180f988b4f9c75334271662
Simon Torres <simon.torres@noirlab.edu>
2025-07-18 17:25:52 -0400

    Created entrypoint scripts for astrometry and photometry

--- goodman_pipeline/scripts/scripts.py
+++ goodman_pipeline/scripts/scripts.py
@@ -6,6 +6,11 @@ from goodman_pipeline.core import setup_logging
 from goodman_pipeline.images import ReduceCCD
 from goodman_pipeline.spectroscopy import ReduceSpectroscopy
 
+from goodman_pipeline.astrometry import Astrometry
+from goodman_pipeline.astrometry.utils import get_astrometry_config_args
+
+from goodman_pipeline.photometry import Photometry
+
 import sys
 
 if '-h' not in sys.argv and \
@@ -21,3 +26,25 @@ def redccd():  # pragma: no cover
 def redspec():  # pragma: no cover
     reduce_spectroscopy = ReduceSpectroscopy()
     reduce_spectroscopy()
+
+
+def redastrometry():
+    args = get_astrometry_config_args()
+    astrometry = Astrometry(pixel_scale=args.pixel_scale,
+                            pixel_scale_tolerance=args.pixel_scale_tolerance,
+                            scale_units=args.scale_units,
+                            downsample_factor=args.downsample_factor,
+                            binning_keyword=args.binning_keyword,
+                            index_directory=args.index_directory,
+                            overwrite=args.overwrite,
+                            debug=args.debug,
+                            verbose=args.verbose)
+
+    astrometry(filename=args.filename, return_json=False)
+
+
+def redphotometry():
+    # args = get_photometry_config_args()
+    # photometry = Photometry()
+    # photometry(filename=args.filename)
+    pass
